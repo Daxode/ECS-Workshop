a5012966c90cca8e2288fc733531e8ce4923a807
Daniel Kierkegaard Andersen <dax@daxode.dk>
2023-09-04 12:45:11 +0200

    More ships + area effect

--- Course-of-a-Boat/Assets/Scripts/BoatAuthor.cs
+++ Course-of-a-Boat/Assets/Scripts/BoatAuthor.cs
@@ -5,6 +5,7 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Extensions;
 using Unity.Transforms;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -17,8 +18,8 @@ public struct FollowMarkerData : IComponentData
 
 public class BoatAuthor : MonoBehaviour
 {
+    
     public GameObject PreviewModel;
-    [FormerlySerializedAs("FollowMarker")]
     public FollowMarkerData FollowMarkerData;
 
     public class BoatBaker : Baker<BoatAuthor>
@@ -49,8 +50,19 @@ partial struct MoveToMarkerSystem : ISystem
         var marker = SystemAPI.GetSingletonEntity<MarkerTag>();
         var markerLT = SystemAPI.GetComponent<LocalTransform>(marker);
 
+        if (!Input.GetKey(KeyCode.Space))
+            return;
+        
         foreach (var (velocityRef, ltRef, massRef, followRef) in SystemAPI.Query<RefRW<PhysicsVelocity>, RefRO<LocalTransform>, RefRO<PhysicsMass>, RefRO<FollowMarkerData>>())
         {
+            // check if we are close enough to marker otherwise stop
+            if (math.distancesq(markerLT.Position.xz, ltRef.ValueRO.Position.xz) > 10*10f)
+            {
+                velocityRef.ValueRW.Linear = float3.zero;
+                velocityRef.ValueRW.Angular = float3.zero;
+                continue;
+            }
+        
             // go forward by speed
             var speed = math.smoothstep(5*5, 6*6, math.distancesq(markerLT.Position.xz, ltRef.ValueRO.Position.xz)); // slows down at 6m to marker, stops at 5m
             velocityRef.ValueRW.Linear = ltRef.ValueRO.Forward() * (speed * SystemAPI.Time.DeltaTime * followRef.ValueRO.speed);
