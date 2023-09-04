f04ac8c95c803cc85de2647a24663a84c4e2ba9d
Daniel Kierkegaard Andersen <dax@daxode.dk>
2023-09-05 01:22:58 +0200

    Understanding Physics

--- Course-of-a-Boat/Assets/Scripts/BoatAuthor.cs
+++ Course-of-a-Boat/Assets/Scripts/BoatAuthor.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
 using UnityEngine;
 
 [Serializable]
-public struct FollowMarkerData
+public struct FollowMarkerData : IComponentData
 {
     public float speed;
     public float turnSpeed;
@@ -12,4 +15,18 @@ public class BoatAuthor : MonoBehaviour
 {
     public GameObject PreviewModel;
     public FollowMarkerData FollowMarkerData;
+
+    class BoatAuthorBaker : Baker<BoatAuthor>
+    {
+        public override void Bake(BoatAuthor authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.None);
+            AddComponent(entity, authoring.FollowMarkerData);
+
+            var joint = CreateAdditionalEntity(TransformUsageFlags.None);
+            AddComponent(joint, PhysicsJoint.CreateLimitedDOF(default, new bool3(false, true, false), new bool3(true,false,true)));
+            AddComponent(joint, new PhysicsConstrainedBodyPair(entity, Entity.Null, false));
+            AddSharedComponent(joint, default(PhysicsWorldIndex));
+        }
+    }
 }
---
+++ Course-of-a-Boat/Assets/Scripts/Runtime/MoveBoatSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Runtime
+{
+    public partial struct MoveBoatSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            if (!Input.GetKey(KeyCode.Space))
+                return;
+            
+            foreach (var (velocityRef, ltRef, followRef) in SystemAPI.Query<RefRW<PhysicsVelocity>, RefRO<LocalTransform>, RefRO<FollowMarkerData>>())
+            {
+                // go forward by speed
+                velocityRef.ValueRW.Linear = ltRef.ValueRO.Forward() * (SystemAPI.Time.DeltaTime * followRef.ValueRO.speed);
+            }
+        }
+    }
+}
