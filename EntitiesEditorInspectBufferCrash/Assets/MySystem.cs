44b3a8bb251ae52e6c7f019abaf3a04018042759
daniel-andersen <daniel.andersen@unity3d.com>
2023-09-07 14:29:14 +0200

    Reproduction Case - Entities Editor Inspect Buffer Crash

---
+++ EntitiesEditorInspectBufferCrash/Assets/MySystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public struct SomeElement : IBufferElementData
+{
+    public int Value;
+}
+    
+public partial struct MySystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        var buffer = state.EntityManager.AddBuffer<SomeElement>(state.EntityManager.CreateEntity());
+        buffer .Add(new SomeElement { Value = 5 });
+        buffer .Add(new SomeElement { Value = 10 });
+        buffer .Add(new SomeElement { Value = 15 });
+    }
+}
