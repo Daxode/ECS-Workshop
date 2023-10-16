96fbce217a419f5fd0ace4c7e32543b953acc6b9
Daxode <dax@daxode.dk>
2023-10-17 00:17:56 +0200

    Working movement and animation (even draws in editor lol)

---
+++ New-Start/Assets/AnimationAuthor.cs
@@ -0,0 +1,89 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class AnimationAuthor : MonoBehaviour {
+        [SerializeField] Animator animator;
+        public class AnimationManaged : IComponentData {
+            public Animator animator;
+        }
+        
+        class AnimationAuthorBaker : Baker<AnimationAuthor> {
+            public override void Bake(AnimationAuthor authoring) {
+                var entity = GetEntity(TransformUsageFlags.Renderable);
+                AddComponentObject(entity, new AnimationManaged {
+                    animator = authoring.animator
+                });
+
+                if (IsBakingForEditor()) {
+                    AddComponent(entity, new DrawInEditor {
+                        entity = GetEntity(authoring.animator, TransformUsageFlags.Renderable | TransformUsageFlags.WorldSpace)
+                    });
+                }
+            }
+        }
+
+        internal struct DrawInEditor : IComponentData {
+            public Entity entity;
+        }
+    }
+    
+    [WorldSystemFilter(WorldSystemFilterFlags.Editor)]
+    [UpdateAfter(typeof(TransformSystemGroup))]
+    partial struct SpawnToDrawInEditorSystem : ISystem, ISystemStartStop {
+        public void OnCreate(ref SystemState state) {
+            state.RequireForUpdate<AnimationAuthor.DrawInEditor>();
+        }
+        public void OnStartRunning(ref SystemState state) {
+            var ecb = SystemAPI
+                .GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged);
+            foreach (var (drawnEntities, parent) in SystemAPI.Query<AnimationAuthor.DrawInEditor>().WithEntityAccess()) {
+                var e = state.EntityManager.Instantiate(drawnEntities.entity);
+                if (SystemAPI.HasComponent<Parent>(e)) {
+                    SystemAPI.SetComponent(e, new Parent {Value = parent});
+                } else {
+                    ecb.AddComponent(e, new Parent {Value = parent});
+                }
+            }
+            state.EntityManager.RemoveComponent<AnimationAuthor.DrawInEditor>(
+                    SystemAPI.QueryBuilder().WithAll<AnimationAuthor.DrawInEditor>().Build());
+        }
+
+        public void OnStopRunning(ref SystemState state) {
+            
+        }
+    }
+    
+    
+    partial struct AnimationSystem : ISystem, ISystemStartStop {
+        public void OnCreate(ref SystemState state) {
+            state.RequireForUpdate<AnimationAuthor.AnimationManaged>();
+        }
+        public void OnStartRunning(ref SystemState state) {
+            var ecb = SystemAPI
+                .GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged);
+            
+            foreach (var (animationManaged, entity) in SystemAPI.Query<AnimationAuthor.AnimationManaged>().WithEntityAccess()) {
+                var animatorInstance = Object.Instantiate(animationManaged.animator);
+                ecb.AddComponent(entity, animatorInstance);
+            }
+        }
+
+        public void OnStopRunning(ref SystemState state) {}
+    }
+    
+    [UpdateAfter(typeof(TransformSystemGroup))]
+    partial struct SyncTransformWithGOSystem : ISystem {
+        public void OnUpdate(ref SystemState state) {
+            foreach (var (ltw, animator) in 
+                     SystemAPI.Query<RefRO<LocalToWorld>, SystemAPI.ManagedAPI.UnityEngineComponent<Animator>>()) {
+                var animatorTransform = animator.Value.transform;
+                animatorTransform.SetPositionAndRotation(ltw.ValueRO.Position, ltw.ValueRO.Rotation);
+            }
+        }
+    }
+}
+
---
+++ New-Start/Assets/PlayerMovementAuthor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.InputSystem;
+// Authoring Data
+public class PlayerMovementAuthor : MonoBehaviour {
+    [SerializeField] InputAction directionalInput;
+    [SerializeField] float speed;
+    [SerializeField] Transform model;
+    
+    public class PlayerInputManaged : IComponentData {
+        public InputAction directionalInput;
+        public float speed;
+        public Entity modelEntity;
+    }
+    
+    class Baker : Baker<PlayerMovementAuthor> {
+        public override void Bake(PlayerMovementAuthor authoring) {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponentObject(entity, new PlayerInputManaged {
+                directionalInput = authoring.directionalInput,
+                speed = authoring.speed,
+                modelEntity = authoring.model ? GetEntity(authoring.model, TransformUsageFlags.Dynamic) : Entity.Null
+            });
+
+            // lock to xz plane
+            var jointEntity = CreateAdditionalEntity(TransformUsageFlags.None);
+            AddComponent(jointEntity, PhysicsJoint.CreateLimitedDOF(RigidTransform.identity,
+                    false, true));
+            AddComponent(jointEntity, new PhysicsConstrainedBodyPair(entity, Entity.Null, false));
+            AddComponent<PhysicsWorldIndex>(jointEntity);
+        }
+    }
+}
+
+partial struct MyInitSystem : ISystem, ISystemStartStop {
+    static readonly int k_Blend = Animator.StringToHash("Blend");
+
+    public void OnCreate(ref SystemState state) {
+        state.RequireForUpdate<PlayerMovementAuthor.PlayerInputManaged>();
+    }
+
+    public void OnStartRunning(ref SystemState state) {
+        foreach (var playerInputManaged in SystemAPI.Query<PlayerMovementAuthor.PlayerInputManaged>()) {
+            playerInputManaged.directionalInput.Enable();
+        }
+    }
+
+    public void OnStopRunning(ref SystemState state)
+    {
+        foreach (var playerInputManaged in SystemAPI.Query<PlayerMovementAuthor.PlayerInputManaged>()) {
+            playerInputManaged.directionalInput.Disable();
+        }
+    }
+
+    public void OnUpdate(ref SystemState state) {
+        var timeData = SystemAPI.Time;
+        foreach (var (playerInputManaged, velocityRef) in SystemAPI.Query<PlayerMovementAuthor.PlayerInputManaged, RefRW<PhysicsVelocity>>()) {
+            var input = playerInputManaged.directionalInput.ReadValue<Vector2>();
+            var preserveY = velocityRef.ValueRO.Linear.y;
+            velocityRef.ValueRW.Linear = new float3(input.x, 0, input.y) * playerInputManaged.speed * timeData.DeltaTime;
+            velocityRef.ValueRW.Linear.y = preserveY;
+
+            // check if model is Animator and set the Animator's speed
+            if (SystemAPI.ManagedAPI.HasComponent<Animator>(playerInputManaged.modelEntity)) {
+                var animator = SystemAPI.ManagedAPI.GetComponent<Animator>(playerInputManaged.modelEntity);
+                animator.SetFloat(k_Blend, math.length(velocityRef.ValueRO.Linear));
+            }
+        }
+    }
+}
+
+partial struct RotateModelToPhysicsVel : ISystem {
+    public void OnUpdate(ref SystemState state) {
+        // loop through all entities with a PhysicsVelocity and PlayerInputManaged
+        foreach (var (velocity, playerInputManaged) in SystemAPI.Query<PhysicsVelocity, PlayerMovementAuthor.PlayerInputManaged>()) {
+            if (math.lengthsq(velocity.Linear) < 0.01f) continue;
+            
+            var modelEntity = playerInputManaged.modelEntity;
+            var modelLT = SystemAPI.GetComponent<LocalTransform>(modelEntity);
+            modelLT.Rotation = math.mul(modelLT.Rotation,
+                quaternion.RotateY(Vector2.SignedAngle(velocity.Linear.xz, modelLT.Forward().xz)*0.001f));
+            SystemAPI.SetComponent(modelEntity, modelLT);
+        }
+    }
+}
