f546edc7c5bd707696954f65dda7b37a8b0e93d1
Daxode <dax@daxode.dk>
2023-10-24 19:14:59 +0200

    Make an Entity - Task

---
+++ New-Start/Assets/Scripts/Runtime/LifetimeSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+struct Lifetime : IComponentData
+{
+    public float timeLeft;
+}
+
+partial struct LifetimeSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        // create projectile 1
+        var projectileEntity1 = state.EntityManager.CreateEntity();
+        state.EntityManager.AddComponentData(projectileEntity1, new Lifetime
+        {
+            timeLeft = 1.0f
+        });
+        state.EntityManager.SetName(projectileEntity1, "Projectile 1");
+        
+        // create another projectile
+        var projectileEntity2 = state.EntityManager.CreateEntity();
+        state.EntityManager.AddComponentData(projectileEntity2, new Lifetime
+        {
+            timeLeft = 2.0f
+        });
+        state.EntityManager.SetName(projectileEntity2, "Projectile 2");
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        // create an entity command buffer to destroy entities
+        var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
+        
+        // destroy projectiles when their lifetime is over
+        foreach (var (lifetimeRef, e) in SystemAPI.Query<RefRW<Lifetime>>().WithEntityAccess())
+        {
+            lifetimeRef.ValueRW.timeLeft -= SystemAPI.Time.DeltaTime;
+            if (lifetimeRef.ValueRW.timeLeft <= 0f)
+            {
+                ecb.DestroyEntity(e);
+                Debug.Log($"Lifetime over for {state.EntityManager.GetName(e)}");
+            }
+        }
+        
+        // play back the entity command buffer
+        ecb.Playback(state.EntityManager);
+    }
+}
