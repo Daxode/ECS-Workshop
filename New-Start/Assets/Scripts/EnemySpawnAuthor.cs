42c30774163bf9a975c0602c3e305e64767a796b
Daxode <dax@daxode.dk>
2023-10-24 20:37:33 +0200

    Instantiate Prefab - Task

--- New-Start/Assets/Scripts/EnemySpawnAuthor.cs
+++ New-Start/Assets/Scripts/EnemySpawnAuthor.cs
@@ -1,7 +1,22 @@
-﻿using UnityEngine;
+﻿using Unity.Entities;
+using UnityEngine;
 
 public class EnemySpawnAuthor : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] float spawnInterval;
+    
+    class Baker : Baker<EnemySpawnAuthor>
+    {
+        public override void Bake(EnemySpawnAuthor authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Renderable);
+            AddComponent(entity, new SpawnerData
+            {
+                enemyPrefab = GetEntity(authoring.enemyPrefab, TransformUsageFlags.Dynamic),
+                spawnInterval = authoring.spawnInterval,
+                spawnTimer = authoring.spawnInterval
+            });
+        }
+    }
 }
---
+++ New-Start/Assets/Scripts/Runtime/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+struct SpawnerData : IComponentData
+{
+    public Entity enemyPrefab;
+    public float spawnInterval;
+    public float spawnTimer;
+}
+
+public partial struct EnemySpawner : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (dataRef, ltw) in SystemAPI.Query<RefRW<SpawnerData>, RefRO<LocalToWorld>>())
+        {
+            if (dataRef.ValueRO.spawnTimer > 0)
+                dataRef.ValueRW.spawnTimer -= SystemAPI.Time.DeltaTime;
+            else
+            {
+                dataRef.ValueRW.spawnTimer = dataRef.ValueRO.spawnInterval;
+                var instance = state.EntityManager.Instantiate(dataRef.ValueRO.enemyPrefab);
+                SystemAPI.SetComponent(instance, new LocalTransform
+                {
+                    Position = ltw.ValueRO.Position,
+                    Rotation = ltw.ValueRO.Rotation,
+                    Scale = 1f
+                });
+            }
+        }
+    }
+}
