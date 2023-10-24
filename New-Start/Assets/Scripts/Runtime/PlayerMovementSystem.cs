5e336d7687a4eb84e0356562537049e588f21793
Daxode <dax@daxode.dk>
2023-10-24 19:02:42 +0200

    Make an Entity

---
+++ New-Start/Assets/Scripts/Runtime/PlayerMovementSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+struct PlayerMovement : IComponentData
+{
+    public float speed;
+}
+
+partial struct PlayerMovementSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        var playerEntity = state.EntityManager.CreateEntity();
+        state.EntityManager.AddComponentData(playerEntity,  new PlayerMovement
+        {
+            speed = 1.0f
+        });
+        state.EntityManager.SetName(playerEntity, "Player");
+        state.EntityManager.AddComponentData(playerEntity, LocalTransform.Identity);
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (playerMovementRef, localTransformRef) in SystemAPI.Query<RefRO<PlayerMovement>, RefRW<LocalTransform>>())
+        {
+            var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            var velocity = new float3(input.x, 0, input.y) * playerMovementRef.ValueRO.speed;
+            localTransformRef.ValueRW.Position += velocity * SystemAPI.Time.DeltaTime;
+            Debug.Log($"Player position: {localTransformRef.ValueRW.Position}");
+        }
+    }
+}
