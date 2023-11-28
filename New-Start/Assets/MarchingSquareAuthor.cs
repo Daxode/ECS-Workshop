a06a5a8ce0fe8372b71f60f6cec7ddd8c0e45475
Daniel Kierkegaard Andersen <dax@daxode.dk>
2023-11-28 23:33:46 +0100

    working multiple materials!

--- New-Start/Assets/MarchingSquareAuthor.cs
+++ New-Start/Assets/MarchingSquareAuthor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
@@ -11,7 +12,7 @@ using UnityEngine;
 class MarchingSquareAuthor : MonoBehaviour
 {
     public MarchSquareSetSprites[] sets;
-    public UberOverride spriteTargetPrefab;
+    public MarchingSquareTile spriteTargetPrefab;
 }
 
 [Serializable]
@@ -61,6 +62,8 @@ class MarchingSquareBaker : Baker<MarchingSquareAuthor>
             spriteTargetPrefab = GetEntity(authoring.spriteTargetPrefab, TransformUsageFlags.Renderable),
         });
         var buffer = AddBuffer<MarchSquareSet>(entity);
+
+        var textureSheetTexelSize = authoring.spriteTargetPrefab.spriteTextureSheet.texelSize;
         
         foreach (var set in authoring.sets)
         {
@@ -69,22 +72,22 @@ class MarchingSquareBaker : Baker<MarchingSquareAuthor>
             
             buffer.Add(new MarchSquareSet
             {
-                offset0 = set.sprites[0].rect.position,
-                offset1 = set.sprites[1].rect.position,
-                offset2 = set.sprites[2].rect.position,
-                offset3 = set.sprites[3].rect.position,
-                offset4 = set.sprites[4].rect.position,
-                offset5 = set.sprites[5].rect.position,
-                offset6 = set.sprites[6].rect.position,
-                offset7 = set.sprites[7].rect.position,
-                offset8 = set.sprites[8].rect.position,
-                offset9 = set.sprites[9].rect.position,
-                offsetA = set.sprites[10].rect.position,
-                offsetB = set.sprites[11].rect.position,
-                offsetC = set.sprites[12].rect.position,
-                offsetD = set.sprites[13].rect.position,
-                offsetE = set.sprites[14].rect.position,
-                offsetF = set.sprites[15].rect.position,
+                offset0 = set.sprites[0].rect.position * textureSheetTexelSize,
+                offset1 = set.sprites[1].rect.position * textureSheetTexelSize,
+                offset2 = set.sprites[2].rect.position * textureSheetTexelSize,
+                offset3 = set.sprites[3].rect.position * textureSheetTexelSize,
+                offset4 = set.sprites[4].rect.position * textureSheetTexelSize,
+                offset5 = set.sprites[5].rect.position * textureSheetTexelSize,
+                offset6 = set.sprites[6].rect.position * textureSheetTexelSize,
+                offset7 = set.sprites[7].rect.position * textureSheetTexelSize,
+                offset8 = set.sprites[8].rect.position * textureSheetTexelSize,
+                offset9 = set.sprites[9].rect.position * textureSheetTexelSize,
+                offsetA = set.sprites[10].rect.position * textureSheetTexelSize,
+                offsetB = set.sprites[11].rect.position * textureSheetTexelSize,
+                offsetC = set.sprites[12].rect.position * textureSheetTexelSize,
+                offsetD = set.sprites[13].rect.position * textureSheetTexelSize,
+                offsetE = set.sprites[14].rect.position * textureSheetTexelSize,
+                offsetF = set.sprites[15].rect.position * textureSheetTexelSize,
             });
         }
     }
@@ -102,8 +105,10 @@ partial struct MarchSquareSystem : ISystem, ISystemStartStop
         {
             var x = i % k_MarchSquareLookupSize.x;
             var y = i / k_MarchSquareLookupSize.x;
-            var val = noise.cnoise(new float2(x, -y)*0.1f);
-            marchSquareLookup[i] = (int)math.round(val * 4f);
+            var water = math.select(0, 3, noise.cnoise(new float2(x, -y)*0.1f)>0.4f);
+            var ore = math.select(0, 2, noise.cnoise(new float3(x, -y, 0.2f)*0.1f)>0.2f);
+            var air = math.select(0, 1, noise.cnoise(new float3(x, -y, 0.4f)*0.1f)>0.3f);
+            marchSquareLookup[i] = math.min(math.max(water, ore), air);
             
             Debug.DrawLine(new float3(x, -y, 0), new float3(x, -y, 0) + new float3(0, 0, marchSquareLookup[i]), Color.red, 100f);
         }
@@ -118,31 +123,121 @@ partial struct MarchSquareSystem : ISystem, ISystemStartStop
     public void OnStartRunning(ref SystemState state)
     {
         var spriteTargetPrefab = SystemAPI.GetSingleton<MarchSquareData>().spriteTargetPrefab;
-        var marchSquareSets = SystemAPI.GetSingletonBuffer<MarchSquareSet>();
         
         // loop over 2x2 points
         for (int y = 0; y > -k_MarchSquareLookupSize.y + 1; y--)
         {
             for (int x = 0; x < k_MarchSquareLookupSize.x - 1; x++)
             {
-                // get the 4 corners
-                var i = x + -y * k_MarchSquareLookupSize.x;
-                var valTL = (int) math.saturate(marchSquareLookup[i]);
-                var valTR = (int) math.saturate(marchSquareLookup[i + 1]);
-                var valBL = (int) math.saturate(marchSquareLookup[i + k_MarchSquareLookupSize.x]);
-                var valBR = (int) math.saturate(marchSquareLookup[i + k_MarchSquareLookupSize.x + 1]);
-                
-                // build a 4-bit value from the 4 corners
-                var valCombined = valBL | (valBR << 1) | (valTR << 2) | (valTL << 3);
-                var sprite = marchSquareSets[0].GetOffset(valCombined);
-                
                 // spawn a sprite with the correct offset
                 var spriteTarget = state.EntityManager.Instantiate(spriteTargetPrefab);
-                var originalOffset = SystemAPI.GetComponent<MaterialOverrideOffsetXYScaleZW>(spriteTargetPrefab);
                 SystemAPI.SetComponent(spriteTarget, LocalTransform.FromPosition(new float3(x, y, 0)));
-                SystemAPI.SetComponent(spriteTarget, new MaterialOverrideOffsetXYScaleZW { Value = new float4(sprite*originalOffset.Value.zw, 32*originalOffset.Value.zw) });
             }
         }
+    }
+
+    //[BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var marchSquareSets = SystemAPI.GetSingletonBuffer<MarchSquareSet>();
+        foreach (var (lt, offsetXYScaleZwRef) in SystemAPI.Query<LocalToWorld, RefRW<MaterialOverrideOffsetXYScaleZW>>().WithAll<MarchingSquareTileCarverTag>())
+        {
+            var corners = GetCornerValues((int2)lt.Position.xy);
+
+            // build a 4-bit value from the 4 corners
+            corners = 1 - (int4) math.saturate(corners);
+            var valCombined = corners.x | (corners.y << 1) | (corners.z << 2) | (corners.w << 3);
+            offsetXYScaleZwRef.ValueRW.Value.xy = marchSquareSets[0].GetOffset(valCombined);
+        }
+        
+        foreach (var (lt, offsetXYScaleZwRef, cornerStrengthRef) in SystemAPI.Query<LocalToWorld, RefRW<MaterialOverrideOffsetXYScaleZW>, RefRW<MaterialOverrideCornerStrength>>().WithAll<MarchingSquareTileMatATag>())
+        {
+            var corners = GetCornerValues((int2)lt.Position.xy);
+            var highestCorners = SortTheFourNumbers(corners);
+            cornerStrengthRef.ValueRW.Value = (float4) (corners == highestCorners.w);
+            
+            // build a 4-bit value from the 4 corners
+            corners = 1 - (int4) math.saturate(corners);
+            var valCombined = corners.x | (corners.y << 1) | (corners.z << 2) | (corners.w << 3);
+            offsetXYScaleZwRef.ValueRW.Value.xy = marchSquareSets[math.clamp(highestCorners.w-1, 0, marchSquareSets.Length-1)].GetOffset(valCombined);
+            
+        }
+        
+        foreach (var (lt, offsetXYScaleZwRef, cornerStrengthRef) in SystemAPI.Query<LocalToWorld, RefRW<MaterialOverrideOffsetXYScaleZW>, RefRW<MaterialOverrideCornerStrength>>().WithAll<MarchingSquareTileMatBTag>())
+        {
+            var corners = GetCornerValues((int2)lt.Position.xy);
+            var highestCorners = SortTheFourNumbers(corners);
+            
+            var validSecond = 0;
+            if (highestCorners.w != highestCorners.z)
+                validSecond = highestCorners.z;
+            else if (highestCorners.w != highestCorners.y)
+                validSecond = highestCorners.y;
+            else if (highestCorners.w != highestCorners.x)
+                validSecond = highestCorners.x;
+            
+            if (validSecond == 0)
+                cornerStrengthRef.ValueRW.Value = 0;
+            else
+                cornerStrengthRef.ValueRW.Value = (float4) (corners == validSecond);
+            
+            // build a 4-bit value from the 4 corners
+            corners = 1 - (int4) math.saturate(corners);
+            var valCombined = corners.x | (corners.y << 1) | (corners.z << 2) | (corners.w << 3);
+            offsetXYScaleZwRef.ValueRW.Value.xy = marchSquareSets[math.clamp(validSecond-1, 0, marchSquareSets.Length-1)].GetOffset(valCombined);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePosInt = (int2) (math.round(((float3)mousePos).xy+new float2(0.5f, -0.5f)));
+            var i = mousePosInt.x + -mousePosInt.y * k_MarchSquareLookupSize.x;
+            if (Input.GetKey(KeyCode.LeftShift))
+                marchSquareLookup[i] = math.select(2, 3, Input.GetKey(KeyCode.Mouse0));
+            else
+                marchSquareLookup[i] = math.select(0, 1, Input.GetKey(KeyCode.Mouse0));
+        }
+        
+        // camera y up/down from scroll
+        if (Input.mouseScrollDelta != Vector2.zero)
+        {
+            var camera = Camera.main;
+            camera.transform.position += new Vector3(0, Input.mouseScrollDelta.y, 0);
+        }
+    }
+
+    static int4 SortTheFourNumbers(int4 val)
+    {
+        var lowhigh1 = new int2();
+        var lowhigh2 = new int2();
+        var lowestmiddle1 = new int2();
+        var middle2highest = new int2();
+        
+        lowhigh1 = math.select(val.yx, val.xy, val.x < val.y);
+        lowhigh2 = math.select(val.wz, val.zw, val.z < val.w);
+        lowestmiddle1 = math.select(new int2(lowhigh2.x, lowhigh1.x), new int2(lowhigh1.x, lowhigh2.x), lowhigh1.x < lowhigh2.x);
+        middle2highest = math.select(new int2(lowhigh2.y, lowhigh1.y), new int2(lowhigh1.y, lowhigh2.y), lowhigh1.y < lowhigh2.y);
+
+        return math.select(
+            new int4(lowestmiddle1.x,middle2highest.x, lowestmiddle1.y, middle2highest.y), 
+            new int4(lowestmiddle1, middle2highest), 
+            lowestmiddle1.y < middle2highest.x);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    int4 GetCornerValues(int2 coord)
+    {
+        // assert that y is negative or ground level. As we're going from top to bottom, y should be negative
+        if (coord.y > 0) throw new Exception("y must be negative");
+
+        // get the 4 corners
+        var i = coord.x + -coord.y * k_MarchSquareLookupSize.x;
+        return new int4(
+            marchSquareLookup[i + k_MarchSquareLookupSize.x], // Bottom Left
+            marchSquareLookup[i + k_MarchSquareLookupSize.x + 1], // Bottom Right
+            marchSquareLookup[i + 1], // Top Right
+            marchSquareLookup[i] // Top Left
+        );
     }
 
     public void OnStopRunning(ref SystemState state) {}
---
+++ New-Start/Assets/MarchingSquareTileLayer.cs
@@ -0,0 +1,60 @@
+using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MarchingSquareTileLayer : MonoBehaviour
+{
+    public MarchingSquareTagType tagType;
+    public enum MarchingSquareTagType
+    {
+        Carver,
+        MatA,
+        MatB,
+    }
+}
+
+class SpriteBaker : Baker<MarchingSquareTileLayer>
+{
+    public override void Bake(MarchingSquareTileLayer authoring)
+    {
+        var entity = GetEntity(TransformUsageFlags.Renderable);
+        AddComponent(entity, new MaterialOverrideCornerStrength{ Value = 1 });;
+        AddComponent(entity, new MaterialOverrideOffsetXYScaleZW { Value = new float4(
+            0, // offset
+            DependsOn(GetComponentInParent<MarchingSquareTile>().spriteTextureSheet).texelSize * 32 // scale
+        )});
+
+        switch (authoring.tagType)
+        {
+            case MarchingSquareTileLayer.MarchingSquareTagType.Carver:
+                AddComponent(entity, new MarchingSquareTileCarverTag());
+                break;
+            case MarchingSquareTileLayer.MarchingSquareTagType.MatA:
+                AddComponent(entity, new MarchingSquareTileMatATag());
+                break;
+            case MarchingSquareTileLayer.MarchingSquareTagType.MatB:
+                AddComponent(entity, new MarchingSquareTileMatBTag());
+                break;
+        }
+    }
+}
+
+
+[MaterialProperty("_CornerStrength")]
+public struct MaterialOverrideCornerStrength : IComponentData
+{
+    public float4 Value;
+}
+
+[MaterialProperty("_OffsetXYScaleZW")]
+public struct MaterialOverrideOffsetXYScaleZW : IComponentData
+{
+    public float4 Value;
+}
+
+struct MarchingSquareTileCarverTag : IComponentData {}
+struct MarchingSquareTileMatATag : IComponentData {}
+struct MarchingSquareTileMatBTag : IComponentData {}
